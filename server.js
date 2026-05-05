0840d34e81e0b0607e194bad0ac96ba59282771c
Denal <oldiacil3@gmail.com>
2026-05-06 01:35:23 +0700

    ferf

--- server.js
+++ server.js
@@ -67,3 +67,6 @@ app.get('/api/check-status', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('API Sentral berjalan...'));
+
+// TAMBAHKAN BARIS INI UNTUK VERCEL
+module.exports = app;
